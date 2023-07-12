1394414b3205234a48ccbd3893caa185d07415c9
Rsims04 <rtsims04@gmail.com>
2023-07-12 16:16:49 +0930

    feat: manage users and edit profile page added

--- evently/src/app/app-routing.module.ts
+++ evently/src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { SettingsComponent } from './pages/settings/settings.component';
+import { ManageUsersComponent } from './pages/manage-users/manage-users.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 const signInModule = () =>
   import('../app/pages/sign-in/sign-in.module').then((x) => x.SignInModule);
@@ -20,6 +22,14 @@ const routes: Routes = [
   {
     path: 'settings',
     component: SettingsComponent,
+  },
+  {
+    path: 'admin',
+    component: ManageUsersComponent,
+  },
+  {
+    path: 'edit-profile',
+    component: ProfileComponent,
   },
 ];
 
--- evently/src/app/app.module.ts
+++ evently/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { SettingsComponent } from './pages/settings/settings.component';
 import { CoreModule } from './core/core.module';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { ManageUsersComponent } from './pages/manage-users/manage-users.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,8 @@ import { SignInComponent } from './pages/sign-in/sign-in.component';
     HomeComponent,
     SettingsComponent,
     SignInComponent,
+    ManageUsersComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
