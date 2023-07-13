5d4c71d74afdd4ad7a4c6a60815036e0e4b3a3ca
Rsims04 <rtsims04@gmail.com>
2023-07-13 15:55:00 +0930

    feat: added firebase

--- evently/src/app/app.module.ts
+++ evently/src/app/app.module.ts
@@ -11,6 +11,10 @@ import { CoreModule } from './core/core.module';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { ManageUsersComponent } from './pages/manage-users/manage-users.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -28,6 +32,9 @@ import { ProfileComponent } from './pages/profile/profile.component';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ evently/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'evently-angular',
+    appId: '1:420426665123:web:1e9b9cf2c09be80a064316',
+    databaseURL: 'https://evently-angular-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'evently-angular.appspot.com',
+    apiKey: 'AIzaSyCVy_ggGnIMImyKDkLkZsgMtxB5pxxwF6c',
+    authDomain: 'evently-angular.firebaseapp.com',
+    messagingSenderId: '420426665123',
+    measurementId: 'G-H3XE81HLQ0',
+  },
+};
