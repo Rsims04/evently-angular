7ffb5ed56166665e4b924f2e9eb8b8b6aa17e8e6
Rsims04 <rtsims04@gmail.com>
2023-08-14 19:39:13 +0930

    feat: change email working

--- evently/src/app/pages/profile/profile-dialog/profile-dialog.component.ts
+++ evently/src/app/pages/profile/profile-dialog/profile-dialog.component.ts
@@ -2,7 +2,7 @@ import { DialogModule } from '@angular/cdk/dialog';
 import { Component, Inject, ViewEncapsulation } from '@angular/core';
 import { Firestore, collection, doc, getDocs, onSnapshot, query, updateDoc, where } from '@angular/fire/firestore';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { getAuth, updateProfile } from 'firebase/auth';
+import { getAuth, updateEmail, updateProfile } from 'firebase/auth';
 
 @Component({
   selector: 'app-profile-dialog',
@@ -32,6 +32,19 @@ export class ProfileDialogComponent {
     const user = auth.currentUser;
 
     if (user) {
+      if (this.field === 'email') {
+        const auth = getAuth();
+        updateEmail(user, detail).then(() => {
+          // Email updated!
+          console.log('email updated to:',detail);
+          // ...
+        }).catch((error) => {
+          // An error occurred
+          // ...
+          console.log('Error changing email: ',error.message);
+        });
+      }
+
       const q = query(collection(this.db, 'User'), where('uid', '==', user.uid));
       
       const querySnapshot = await getDocs(q);
